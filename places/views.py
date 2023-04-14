347b845133bbfd1616d5b41e39f2d8c59fc992eb
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-14 23:51:36 +0500

    fetching data from database have been added

--- places/views.py
+++ places/views.py
@@ -1,37 +1,30 @@
 from django.shortcuts import render
 
+from places.models import Place
+
 
 def show_index_page(request):
     context = {
         'place_descriptions': {
             "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                    },
-                    "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
-                    }
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [37.64, 55.753676]
-                    },
-                    "properties": {
-                        "title": "Крыши24.рф",
-                        "placeId": "roofs24",
-                        "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-                    }
-                }
-            ]
+            "features": []
         }    
     }
+
+    places = Place.objects.all()
+    for place in places:
+        place_feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "coming_soon"
+            }
+        }
+        context['place_descriptions']['features'].append(place_feature)
 
     return render(request, 'index.html', context=context)
