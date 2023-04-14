f0acae9236860b6d77d7f30a2350116e1010cc51
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-14 23:27:41 +0500

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
