c03e02835ea6801c4d6d315d93a8bf844edd91bf
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-15 12:35:51 +0500

    fetching data from DB finally completed

--- places/views.py
+++ places/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
+from django.urls import reverse
 
 from places.models import Place
 
@@ -8,23 +9,23 @@ from places.models import Place
 def show_index_page(request):
     context = {
         'place_descriptions': {
-            "type": "FeatureCollection",
-            "features": []
+            'type': 'FeatureCollection',
+            'features': []
         }
     }
 
     places = Place.objects.all()
     for place in places:
         place_feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.longitude, place.latitude]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": "coming_soon"
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': reverse('json_place_detail', args=[place.id])
             }
         }
         context['place_descriptions']['features'].append(place_feature)
@@ -35,13 +36,13 @@ def show_index_page(request):
 def show_place_detail(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
     context = {
-        "title": place.title,
-        "imgs": [item.image.url for item in place.images.all()],
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lat": place.latitude,
-            "lng": place.longitude
+        'title': place.title,
+        'imgs': [item.image.url for item in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.latitude,
+            'lng': place.longitude
         }
     }
     return JsonResponse(
