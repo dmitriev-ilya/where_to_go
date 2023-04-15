24382177d90e245e3f26bd1de72cfc9f5fa4610e
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-15 13:26:41 +0500

    prewiev image added in places admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return format_html(
+            '<img src="{url}" width="200" />'.format(
+                url=obj.image.url
+            )
+        )
+    fields = ('image', 'preview_image', 'sequence_number')
 
 
 @admin.register(Place)
