6cf546b5ca4810e853070cda7f9287bafab84f1a
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-16 18:59:19 +0500

    html editor added in admin panel

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Заголовок', max_length=200, blank=False)
     description_short = models.CharField('Краткое описание', max_length=300)
-    description_long = models.TextField('Полное описание')
+    description_long = HTMLField('Полное описание', blank=True)
     longitude = models.DecimalField('Долгота', max_digits=17, decimal_places=14)
     latitude = models.DecimalField('Широта', max_digits=17, decimal_places=14)
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +9,6 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_index_page),
-    path('places/<int:place_id>/', views.show_place_detail, name='json_place_detail')
+    path('places/<int:place_id>/', views.show_place_detail, name='json_place_detail'),
+    path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
