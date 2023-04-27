4903a8aa4933c84e08bcd025f0e98c58e3585c7d
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-04-27 22:16:26 +0500

    change sgort description field

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Заголовок', max_length=200, unique=True)
-    description_short = models.CharField('Краткое описание', max_length=300)
+    description_short = models.TextField('Краткое описание', blank=True)
     description_long = HTMLField('Полное описание', blank=True)
     longitude = models.DecimalField('Долгота', max_digits=17, decimal_places=14)
     latitude = models.DecimalField('Широта', max_digits=17, decimal_places=14)
